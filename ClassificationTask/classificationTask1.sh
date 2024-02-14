816267a8b6a6b3d678641b31a7f90e42cb6e7375
Siamias202 <84383708+Siamias202@users.noreply.github.com>
2024-02-14 19:53:02 +0600

    Update classificationTask1.sh

--- ClassificationTask/classificationTask1.sh
+++ ClassificationTask/classificationTask1.sh
@@ -3,35 +3,31 @@ SCRIPTDIR=$(dirname "$0")
 echo $SCRIPTDIR
 
 if [ -z "$1" ]; then
-    echo "Usage: classificationTask.sh 'input_folder' ['outputPredictions.csv']"
+    echo "Usage: classificationTask.sh input_folder [output_folder]"
 else
     input_folder="$1"
-    output_file="$SCRIPTDIR/${2:-outputPredictions.csv}"
+    output_folder="$SCRIPTDIR/output_folder"
 
     if [ ! -d "$input_folder" ]; then
-        echo "Folder $input_folder not found!"
+        echo "Input folder $input_folder not found!"
     else
-        # Create an empty output file if it doesn't exist
-        touch "$output_file"
+        if [ -n "$2" ]; then
+            output_folder="$2"
+        fi
 
-        for csv_file in "$input_folder"/output_chunk_*.csv; do
-            if [ -f "$csv_file" ]; then
-                echo "Processing $csv_file"
+        # Create the output folder if it doesn't exist
+        mkdir -p "$output_folder"
 
-                # Extract features and perform classification for each CSV file
-                java -jar "$SCRIPTDIR/Senti4SD-fast.jar" -F A -i "$csv_file" -W "$SCRIPTDIR/dsm.bin" -oc "$SCRIPTDIR/extractedFeatures.csv" -vd 600
-                Rscript "$SCRIPTDIR/classification.R" "$SCRIPTDIR/extractedFeatures.csv" "$SCRIPTDIR/predictions_temp.csv"
+        for input_file in "$input_folder"/*.csv; do
+            if [ -f "$input_file" ]; then
+                output_file="$output_folder/$(basename "$input_file" .csv)_predictions.csv"
+
+                java -jar "$SCRIPTDIR/Senti4SD-fast.jar" -F A -i "$input_file" -W "$SCRIPTDIR/dsm.bin" -oc "$SCRIPTDIR/extractedFeatures.csv" -vd 600
+
+                Rscript "$SCRIPTDIR/classification.R" "$SCRIPTDIR/extractedFeatures.csv" "$output_file"
+
                 rm "$SCRIPTDIR/extractedFeatures.csv"
-                
-                # Append the output to the final output file
-                cat "$SCRIPTDIR/predictions_temp.csv" >> "$output_file"
-                rm "$SCRIPTDIR/predictions_temp.csv"
             fi
         done
-
-        # Sort the final output file by the "Row" column
-        sort -t, -k1,1 -o "$output_file" "$output_file"
-
-        echo "Output appended and sorted in $output_file"
     fi
 fi
